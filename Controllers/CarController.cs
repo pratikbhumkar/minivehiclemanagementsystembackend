f216823ceb73fbc7a4501147842a70790649d5bc
Pratik Bhumkar <prateekbhumkar@gmail.com>
2020-11-23 10:45:21 +1100

    Initial commit backend

---
+++ Controllers/CarController.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VehicleManagementSystem.Interfaces;
+using VehicleManagementSystem.Models;
+
+namespace VehicleManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase
+    {
+        private readonly ILogger<CarController> _logger;
+        private readonly ICarService _carService;
+        public CarController(ICarService carService, ILogger<CarController> logger)
+        {
+            _carService = carService;
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        [Route("GetAllCars")]
+        public async Task<ActionResult> GetAllCars()
+        {
+            _logger.LogInformation("Get all Cars called");
+            var result = await _carService.GetAllCars();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetCarDetails")]
+        public async Task<ActionResult> GetCarDetails(string id)
+        {
+            _logger.LogInformation("Get Car details called");
+            var result = await _carService.GetCar(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("AddCar")]
+        public ActionResult AddCar([FromBody]Car car)
+        {
+            _logger.LogInformation("AddCar Car called");
+            if (!ModelState.IsValid)
+                return BadRequest("Please check data entered");
+
+            var result = _carService.AddCar(car);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("DeleteCar")]
+        public ActionResult DeleteCar(Car car)
+        {
+            _logger.LogInformation("Remove Car called");
+            if (!ModelState.IsValid)
+                return BadRequest("Please check data entered");
+
+            var result = _carService.DeleteCar(car);
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+        [Route("UpdateCar")]
+        public ActionResult UpdateCar(Car car)
+        {
+            _logger.LogInformation("UpdateCar Car called");
+            if (!ModelState.IsValid)
+                return BadRequest("Please check data entered");
+
+            var result = _carService.UpdateCar(car);
+            return Ok(result);
+        }
+    }
+}
